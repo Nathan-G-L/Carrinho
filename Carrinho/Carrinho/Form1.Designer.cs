6470b59b86ce6a3536d26fb3a359eab8f2300181
Nathan Galinari <84142667+Nathan-G-L@users.noreply.github.com>
2021-05-26 19:46:17 -0300

    Add files via upload

---
+++ Carrinho/Carrinho/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace Carrinho
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btADcmp = new System.Windows.Forms.Button();
+            this.ckbxLista = new System.Windows.Forms.CheckedListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ckbxCarrinho = new System.Windows.Forms.CheckedListBox();
+            this.cbMerc = new System.Windows.Forms.ComboBox();
+            this.btFim = new System.Windows.Forms.Button();
+            this.btRemove = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mercado";
+            // 
+            // btADcmp
+            // 
+            this.btADcmp.Location = new System.Drawing.Point(267, 24);
+            this.btADcmp.Name = "btADcmp";
+            this.btADcmp.Size = new System.Drawing.Size(75, 23);
+            this.btADcmp.TabIndex = 2;
+            this.btADcmp.Text = "Adicionar";
+            this.btADcmp.UseVisualStyleBackColor = true;
+            this.btADcmp.Click += new System.EventHandler(this.btADcmp_Click);
+            // 
+            // ckbxLista
+            // 
+            this.ckbxLista.FormattingEnabled = true;
+            this.ckbxLista.Location = new System.Drawing.Point(412, 12);
+            this.ckbxLista.Name = "ckbxLista";
+            this.ckbxLista.Size = new System.Drawing.Size(120, 94);
+            this.ckbxLista.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ckbxCarrinho);
+            this.groupBox1.Location = new System.Drawing.Point(12, 135);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(508, 111);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Carrinho";
+            // 
+            // ckbxCarrinho
+            // 
+            this.ckbxCarrinho.FormattingEnabled = true;
+            this.ckbxCarrinho.Location = new System.Drawing.Point(6, 19);
+            this.ckbxCarrinho.Name = "ckbxCarrinho";
+            this.ckbxCarrinho.Size = new System.Drawing.Size(496, 79);
+            this.ckbxCarrinho.TabIndex = 4;
+            this.ckbxCarrinho.SelectedValueChanged += new System.EventHandler(this.ckbxCarrinho_SelectedValueChanged);
+            // 
+            // cbMerc
+            // 
+            this.cbMerc.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.cbMerc.BackColor = System.Drawing.SystemColors.Window;
+            this.cbMerc.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cbMerc.FormattingEnabled = true;
+            this.cbMerc.Location = new System.Drawing.Point(67, 26);
+            this.cbMerc.Name = "cbMerc";
+            this.cbMerc.Size = new System.Drawing.Size(194, 21);
+            this.cbMerc.TabIndex = 1;
+            // 
+            // btFim
+            // 
+            this.btFim.Location = new System.Drawing.Point(457, 252);
+            this.btFim.Name = "btFim";
+            this.btFim.Size = new System.Drawing.Size(75, 23);
+            this.btFim.TabIndex = 5;
+            this.btFim.Text = "Finalizar";
+            this.btFim.UseVisualStyleBackColor = true;
+            this.btFim.Click += new System.EventHandler(this.btFim_Click);
+            // 
+            // btRemove
+            // 
+            this.btRemove.Location = new System.Drawing.Point(15, 252);
+            this.btRemove.Name = "btRemove";
+            this.btRemove.Size = new System.Drawing.Size(75, 23);
+            this.btRemove.TabIndex = 6;
+            this.btRemove.Text = "Remover";
+            this.btRemove.UseVisualStyleBackColor = true;
+            this.btRemove.Visible = false;
+            this.btRemove.Click += new System.EventHandler(this.btRemove_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 279);
+            this.Controls.Add(this.btRemove);
+            this.Controls.Add(this.btFim);
+            this.Controls.Add(this.cbMerc);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.ckbxLista);
+            this.Controls.Add(this.btADcmp);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Carrinho";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btADcmp;
+        private System.Windows.Forms.CheckedListBox ckbxLista;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckedListBox ckbxCarrinho;
+        private System.Windows.Forms.ComboBox cbMerc;
+        private System.Windows.Forms.Button btFim;
+        private System.Windows.Forms.Button btRemove;
+    }
+}
+
---
+++ Carrinho/Carrinho/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Carrinho
+{
+    public partial class Form1 : Form
+    {
+        #region strings
+        string[,] Lista = new string[2, 5];
+        string[,] Mercado = new string[4, 10];
+        string[,] Carrinho = new string[3, 20];
+
+        
+
+        #endregion
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            ListComp();
+            EstoqueM();
+        }
+
+        #region Arrays
+        public void ListComp()
+        {
+
+            //Itens a comprar
+            Lista[0, 0] = "Pão";
+            Lista[0, 1] = "Maçã";
+            Lista[0, 2] = "Maionese";
+            Lista[0, 3] = "Mortadela";
+            Lista[0, 4] = "Farofa";
+
+            //Quantidade de cada item
+            Lista[1, 0] = "3";
+            Lista[1, 1] = "5";
+            Lista[1, 2] = "1";
+            Lista[1, 3] = "1";
+            Lista[1, 4] = "2";
+
+            string[] Atulist = new string[Lista.GetLength(1)];
+
+            for (int i = 0; i < Lista.GetLength(1); i++)
+            {//Joga itens na lista               
+
+                Atulist[i] = Lista[0, i];
+
+                ckbxLista.Items.Add(Atulist[i]);
+            }
+            
+
+        }
+
+        public void EstoqueM()
+        {
+            //Itens mercado
+            Mercado[0, 0] = "Pão";
+            Mercado[0, 1] = "Maçã";
+            Mercado[0, 2] = "Maionese";
+            Mercado[0, 3] = "Mortadela";
+            Mercado[0, 4] = "Farofa";
+            Mercado[0, 5] = "Macarrão";
+            Mercado[0, 6] = "Sal";
+            Mercado[0, 7] = "Salgadinho";
+            Mercado[0, 8] = "Coca-Cola";
+            Mercado[0, 9] = "Shoyu";
+
+            //Valor und
+            Mercado[1, 0] = "1";
+            Mercado[1, 1] = "1";
+            Mercado[1, 2] = "1";
+            Mercado[1, 3] = "1";
+            Mercado[1, 4] = "1";
+            Mercado[1, 5] = "1";
+            Mercado[1, 6] = "1";
+            Mercado[1, 7] = "1";
+            Mercado[1, 8] = "1";
+            Mercado[1, 9] = "1";
+
+            //Valor Estoque
+            Mercado[2, 0] = "20";
+            Mercado[2, 1] = "50";
+            Mercado[2, 2] = "5";
+            Mercado[2, 3] = "5";
+            Mercado[2, 4] = "1";
+            Mercado[2, 5] = "10";
+            Mercado[2, 6] = "5";
+            Mercado[2, 7] = "2";
+            Mercado[2, 8] = "0";
+            Mercado[2, 9] = "3";
+
+            //Peso Itens
+            Mercado[3, 0] = "0.8";
+            Mercado[3, 1] = "0.5";
+            Mercado[3, 2] = "0.5";
+            Mercado[3, 3] = "0.1";
+            Mercado[3, 4] = "0.2";
+            Mercado[3, 5] = "0.8";
+            Mercado[3, 6] = "1.0";
+            Mercado[3, 7] = "0.08";
+            Mercado[3, 8] = "2.0";
+            Mercado[3, 9] = "0.2";
+
+            string[] Atulist = new string[Mercado.GetLength(1)];
+
+            for (int i = 0; i < Mercado.GetLength(1); i++)
+            {//Joga itens no mercado               
+
+                Atulist[i] = Mercado[0, i];
+
+                cbMerc.Items.Add(Atulist[i]);
+            }
+
+        }
+
+
+        #endregion
+
+        #region Botões
+        private void btADcmp_Click(object sender, EventArgs e)
+        {
+            string Mercitem = cbMerc.SelectedItem.ToString();
+
+            for(int i = 0; i < Carrinho.GetLength(1); i++)
+            {
+                if (Carrinho[0, i] == "" || Carrinho[0,i]==null)
+                {
+                    Carrinho[0, i] = Mercitem;
+                    
+                    ckbxCarrinho.Items.Add(Carrinho[0, i]);//Adiciona item ao carrinho
+
+                    //Tira item do estoque
+                    for (int y = 0; y < Mercado.GetLength(1); y++)
+                    {
+                        if (Mercado[0, y] == Mercitem)
+                        {
+                            int TiraEst= int.Parse(Mercado[2, y]);
+                            Mercado[2, y] = (TiraEst - int.Parse(Mercado[1,y])).ToString();
+                                                     
+
+                            Carrinho[1, i] =( int.Parse(Mercado[1, y])).ToString();
+                        }
+                    }                   
+
+                    break;
+                }
+                
+            }
+            //Verifica lista de compras
+            for (int y = 0; y < Lista.GetLength(1);y++)
+            {
+
+                int total = 0;
+                for (int i = 0; i < Carrinho.GetLength(1); i++)
+                {
+                    if (Lista[0, y] == Carrinho[0, i])
+                    {
+                        total ++;
+                        if (Lista[1, y] == total.ToString())
+                        {
+                            ckbxLista.SetItemChecked(y, true);
+                        }
+                    }
+
+                }
+            }
+        }
+        private void btRemove_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < Carrinho.GetLength(1); i++)
+            {
+                if (ckbxCarrinho.SelectedIndex == i)
+                {
+                    string Item = ckbxCarrinho.SelectedItem.ToString();
+                    ckbxCarrinho.Items.RemoveAt(i);
+
+                    Carrinho[0, i] = null;
+                    Carrinho[1, i] = null;
+
+                }
+            }
+        }
+        private void btFim_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        #endregion
+
+        #region Ações
+        private void ckbxCarrinho_SelectedValueChanged(object sender, EventArgs e)
+        {
+            //Retira itens do carrinho
+            for (int i = 0; i < Carrinho.GetLength(1); i++)
+            {
+                if (ckbxCarrinho.SelectedIndex == i)
+                {
+                    string Item = ckbxCarrinho.SelectedItem.ToString();
+                    ckbxCarrinho.Items.RemoveAt(i);
+
+                    Carrinho[0, i] = null;
+                    Carrinho[1, i] = null;
+
+                }
+            }
+
+            //Verifica Lista de compras
+            int a = 0;
+            for (int i = 0; i < Lista.GetLength(1); i++)
+            {
+                if (Lista[0, i] == ckbxCarrinho.SelectedItem.ToString())
+                {                    
+                    a++;
+
+
+                }
+            }
+
+        }
+
+        #endregion
+    }
+}
